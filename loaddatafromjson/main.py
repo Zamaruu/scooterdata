9a1cfb904832ef74d4b411c6de4268c045ccd0ad
Maximilian Ditz <ditzma@gmx.de>
2021-05-18 13:54:25 +0000

    Dataanalysis added

--- loaddatafromjson/main.py
+++ loaddatafromjson/main.py
@@ -1,7 +1,18 @@
 import json
 import os
+import pandas
 from pymongo import MongoClient
-import pandas as pd
+from sqlalchemy import create_engine
+import matplotlib.pyplot as plt
+
+database = 'scooterdaten'
+connection_string = 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false'
+collections = [
+    'ScooterJanuar',
+    'ScooterFebruar',
+    'ScooterMaerz',
+    'ScooterApril',
+]
 
 def readjsonfromdir():
     paths = [
@@ -11,26 +22,19 @@ def readjsonfromdir():
         r'C:\Users\mditz\Desktop\ScooterDaten\ScooterDaten\ScooterApril',
     ]
 
-    collections = [
-        'ScooterJanuar',
-        'ScooterFebruar',
-        'ScooterMaerz',
-        'ScooterApril',
-    ]
     collCount = 0
 
     print('running main...')
-    client = MongoClient('mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false')
-    db = client['scooterdaten']
-    #collection = db.scootercoll
-    print('got client...')
+    client = MongoClient(connection_string)
+    db = client[database]
 
-    #path = r'C:\Users\mditz\Desktop\ScooterDaten\ScooterDaten\ScooterJanuar'
+    print('got client...')
 
     for path in paths:
         os.chdir(path)
+        db[collections[collCount]].drop()
         collection = db[collections[collCount]]
-        collCount= collCount + 1
+        collCount = collCount + 1
         for file_name in [file for file in os.listdir(path) if file.endswith('.json')]:
           with open(path + '\\' + file_name) as json_file:
               print('reading file ' + file_name)
@@ -46,9 +50,42 @@ def readjsonfromdir():
         print('finished reading from ' + path)
     print('finished reading data')
 
+def loadDataFromDB():
+    print('loading data')
+    client = MongoClient(connection_string)
+    db = client[database]
+    collection = db[collections[0]]
 
+    test_subject = collection.find_one()
+    id = test_subject['id']
+    print('Id of the first element in the collection: ' + id)
+
+    x = collection.find({}, {
+        'id': id,
+        'batteryLevel': 1,
+        'lastStateChange': 1
+    })
+
+    # die ersten 10 ergebnisse ausgeben
+
+    df = pandas.DataFrame(list(x[:25]))
+    df.plot(kind="bar", x="lastStateChange", y="batteryLevel")
+    plt.show()
+
+    avarageBatteryLevel = 0
+    for data in x:
+        avarageBatteryLevel += int(x['batteryLevel'])
+
+    print(type(x))
+
+    #avarageBatteryLevel = avarageBatteryLevel / list(x).
+    #print('Durchschnittliche Batterieladung des Scooters: ' + str(avarageBatteryLevel) + '%')
+
+    #engine = create_engine("mongodb:///?Server=MyServer&;Port=27017&Database=ScooterDaten&User=test&Password=Password")
+    #df = pandas.read_sql("SELECT borough, cuisine FROM restaurants WHERE Name = 'Morris Park Bake Shop'", engine)
+    #df.plot(kind="bar", x="borough", y="cuisine")
+    #plt.show()
 
 if __name__ == '__main__':
-    readjsonfromdir()
-    #for dbs in client.list_database_names():
-     #  print(dbs)
+    loadDataFromDB()
+    #readjsonfromdir()
